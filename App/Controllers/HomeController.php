a862c1c07aa4f434296b1e78366db49b598d1c8e
Jairo Ushiña <jago86@gmail.com>
2022-10-10 19:19:18 -0500

    M6. Class 2.

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -3,11 +3,17 @@
 namespace App\Controllers;
 
 use App\Models\Task;
+use simplehtmldom\HtmlWeb;
 
 class HomeController
 {
     public function show()
     {
+        $clientWeb = new HtmlWeb;
+        $html = $clientWeb->load('https://www.amazon.com/s/?field-keywords=bluetooth+keyboards');
+        dd($html->find('#glow-ingress-line2', 0)->innertext);
+        // https://www.amazon.com/s/?field-keywords=bluetooth+keyboards
+
         return view('index');
     }
 }
